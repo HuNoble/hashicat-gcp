82aa6fa792610093c4c06ddc9f51a2fb18901656
Rainbow Dash <rainbow@dash.eq>
2022-03-01 16:07:32 +0000

    Added vpc module 4

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source  = "terraform-google-modules/network/google"
+    source  = "app.terraform.io/abelszabo-training/network/google"
     version = "3.4.0"
 
     project_id   = var.project
